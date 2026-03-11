bae1135d784b092c25259378ceb032bf38b65bda
varletint <intvarlet@hotmail.com>
2026-03-11 14:22:48 +0100

    style: add ambient glow orbs and dot-pattern background to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { FormFill } from "./pages/FormFill";
 import { Success } from "./pages/Success";
@@ -6,8 +5,14 @@ import { Success } from "./pages/Success";
 function App() {
   return (
     <BrowserRouter>
-      <div className='min-h-screen bg-[#0a0b10] text-[#f8fafc] font-sans'>
-        <main className='container mx-auto'>
+      <div className='min-h-screen bg-[#0a0b10] text-[#f8fafc] font-sans bg-dot-pattern relative'>
+        {/* Ambient glow orbs */}
+        <div className='fixed inset-0 pointer-events-none overflow-hidden z-0'>
+          <div className='absolute -top-40 -left-40 w-[500px] h-[500px] rounded-full bg-[#6d28d9] opacity-[0.04] blur-[120px]' />
+          <div className='absolute -bottom-40 -right-40 w-[500px] h-[500px] rounded-full bg-[#6d28d9] opacity-[0.03] blur-[120px]' />
+        </div>
+
+        <main className='relative z-10 w-full'>
           <Routes>
             <Route path='/' element={<Navigate to='/forms/demo' replace />} />
             <Route path='/forms/:formId' element={<FormFill />} />
